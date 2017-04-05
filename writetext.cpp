cbf487afa6b497ac0c10753518ff26d5a07bd768
A1Coder <username@users.noreply.github.com>
2017-04-04 22:33:43 -0300

    write to bin

--- writetext.cpp
+++ writetext.cpp
@@ -9,10 +9,13 @@ int main( )
    float b = 68.123; 
    char c = 'J';
  
-	ofstream file{"abc.txt"};
+	ofstream file{"abc.bin", ios::binary};
 	
 	if(file){
-		file << a << " " << b << " " << c;
+		file.write(reinterpret_cast<char*>(&a), sizeof(a));
+		file.write(reinterpret_cast<char*>(&b),sizeof(b));
+		file.write(reinterpret_cast<char*>(&c),sizeof(c));
+
 	}else{
 		cout << "Error opeing file";
 	}
